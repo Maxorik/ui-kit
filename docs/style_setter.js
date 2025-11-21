bbdaa20901cedf4cb811f7275e43dc877cca38bc
Maxorik <gfifful@gmail.com>
2025-11-21 23:33:18 +0700

    add readme

---
+++ docs/style_setter.js
@@ -0,0 +1,19 @@
+/** Конфиг базовых стилей проекта */
+
+const settings = {
+    /** Основные цвета интерфейса управления */
+    '--primary-btn-color': '#201f26',
+    '--secondary-btn-color': '#83efef',
+    '--hover-btn-color': '#0C1128AD',
+    '--bg-color': '#fff',
+
+    /** Углы */
+    '--corners-standard': '0.65em',
+    '--corners-round': '3em',
+    '--corners-square': '0',
+}
+
+for(let key in settings) {
+    document.documentElement.style.setProperty(key, settings[key]);
+}
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+ui-kit (in progress)
+
+preview: https://maxorik.github.io/ui-kit/
+
+- buttons
+- containers
+- modal window
